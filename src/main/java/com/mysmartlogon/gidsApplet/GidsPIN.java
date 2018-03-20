2f25085cc667da2c047be9b066dd0cdb8ab580b0
Kuldeep Goyal <goyalkuldeep@gmail.com>
2018-03-21 00:29:06 +0100

    Update GidsPIN.java

--- src/main/java/com/mysmartlogon/gidsApplet/GidsPIN.java
+++ src/main/java/com/mysmartlogon/gidsApplet/GidsPIN.java
@@ -31,24 +31,27 @@ import javacard.framework.PIN;
 
 /**
  * \brief The GidsPIN class.
- *
+ * This Class GidsPIN is extends OwnerPIN Class i.e. inherit/override code and extend functionality from this Class . and 
+ implements interface PIN, i.e. implements the function/methods of PIN.
  */
 
 public class GidsPIN extends OwnerPIN implements PIN {
 
 
-    private byte currentPINLen = 0;
-    private byte minPINSize = 0;
-    private byte maxPINSize = 0;
-    private byte tryLimit = 0;
+    private byte currentPINLen = 0;     // Length of Current PIN
+    private byte minPINSize = 0;        // Minimum Pin Size
+    private byte maxPINSize = 0;        // Maximum PIN Size
+    private byte tryLimit = 0;          // Maximum Limit for PIN Try
 
+    // Constructor with three arguments, which are declared above, It sets the values passed as argument.
     public GidsPIN(byte tryLimit, byte maxPINSize, byte minPINSize) {
         super(tryLimit, maxPINSize);
         this.maxPINSize = maxPINSize;
         this.tryLimit = tryLimit;
         this.minPINSize = minPINSize;
     }
-
+    
+    // Four functions to get one variable each from this class.
     public byte GetCurrentPINLen() {
         return currentPINLen;
     }
@@ -60,17 +63,26 @@ public class GidsPIN extends OwnerPIN implements PIN {
     public byte GetMaxPINSize() {
         return maxPINSize;
     }
+    
+    public byte getTryLimit() {
+        return tryLimit;
+    }
 
+    /*
+    // This function checks whether the length of PIN is outside of the Range ( minPINSize and maxPINSize), 
+    // and if yes throw an exception Wrong Length
+    */
     public void CheckLength(byte len) {
         if (len < minPINSize || len > maxPINSize) {
             ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
         }
     }
-
+    
     public void setAsAuthenticated() {
         this.setValidatedFlag(true);
     }
 
+    // This function updates the PIN
     public void update(byte[] pin,
                        short offset,
                        byte length) {
@@ -78,8 +90,5 @@ public class GidsPIN extends OwnerPIN implements PIN {
         currentPINLen = length;
     }
 
-    public byte getTryLimit() {
-        return tryLimit;
-    }
-
+    
 }
