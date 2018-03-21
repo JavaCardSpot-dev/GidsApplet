5d9aeadb9af3f9e6ce2d52e0e3c3d823b8b11c82
dagaoglu <denizagaoglu92@gmail.com>
2018-03-21 21:36:10 +0100

    Update GidsFileSystem.java

--- src/main/java/com/mysmartlogon/gidsApplet/GidsFileSystem.java
+++ src/main/java/com/mysmartlogon/gidsApplet/GidsFileSystem.java
@@ -538,7 +538,9 @@ public class GidsFileSystem extends ApplicationFile {
             File fileToAdd = getSafeFile(buf, ISO7816.OFFSET_CDATA, lc); // getSafeFile performs permission checks.
             addFile(fileToAdd);
             selectFile(fileToAdd);
-        } catch (NotFoundException | InvalidArgumentsException e) {
+        } catch (NotFoundException e) {
+            ISOException.throwIt(ISO7816.SW_DATA_INVALID);
+        } catch (InvalidArgumentsException e) {
             ISOException.throwIt(ISO7816.SW_DATA_INVALID);
         } catch(NotEnoughSpaceException e) {
             ISOException.throwIt(ISO7816.SW_FILE_FULL);
