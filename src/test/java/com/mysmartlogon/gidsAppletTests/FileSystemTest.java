29229b483415381d4cd3ea4db141f9f41a5244fa
Kuldeep Goyal <goyalkuldeep@gmail.com>
2018-04-19 10:00:53 +0200

    getVersion Test Running

--- src/test/java/com/mysmartlogon/gidsAppletTests/FileSystemTest.java
+++ src/test/java/com/mysmartlogon/gidsAppletTests/FileSystemTest.java
@@ -189,18 +189,6 @@ public class FileSystemTest extends GidsBaseTestClass {
         execute("00350001", 0x6986);
         // Command GetVersion APDU
         execute("00350000", "00069000");
-        
-        // Get Response from Command and verify Version 0006
-         ResponseAPDU responseAPDU = execute("00350000", "00069000");
-        
-        /* 
-        byte[] responseBytes = responseAPDU.getBytes();
-         if (responseBytes != null && responseBytes.length == 4) {
-			System.out.println("Applet version " + responseBytes[0] + "."+ responseBytes[1]);
-            //System.out.println("Applet version " + Integer.toHexString(responseBytes[0]) + "."+ Integer.toHexString(responseBytes[1]));
-	    }
-        */            
-			
     }
     
     @Test
